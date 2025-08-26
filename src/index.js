b58617bd963db7820da00ec54840f4ee517a3689
san-melisa <melisasannn@gmail.com>
2025-08-26 10:18:04 +0200

    Add toggle switch for including numbers and symbols

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h"
+    ,"i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 let isActive = false
+let includeNumbers = true
+let includeSymbols = true
+let allCharacters = []
+
+var numberToggle = document.getElementById("number-toggle")
+var symbolToggle = document.getElementById("symbol-toggle")
+
+
+numberToggle.addEventListener("change", function(event) {
+        includeNumbers =  event.target.checked
+})
+
+symbolToggle.addEventListener("change", function(event) {
+        includeSymbols =  event.target.checked
+})
+
 
 function generatePasswords() { 
 
@@ -13,23 +33,38 @@ function generatePasswords() {
     {
         firstPassword.textContent = " "
         secondPassword.textContent = " "
+        allCharacters.length = 0
+
     }
+    allCharacters.push(...characters)
+    
+    if(includeNumbers === true) {
+        allCharacters.push(...numbers)
+    }
+
+    if(includeSymbols === true){
+        allCharacters.push(...symbols)
+    }
+   
         let firstRandom 
         let secondRandom 
 
 
         for(let i = 0 ; i < 15 ; i ++)
         {
-            firstRandom = Math.floor(Math.random() * characters.length)
-            secondRandom = Math.floor(Math.random() * characters.length)
+            firstRandom = Math.floor(Math.random() * allCharacters.length)
+            secondRandom = Math.floor(Math.random() * allCharacters.length)
             
-            firstPassword.textContent += characters[firstRandom]
-            secondPassword.textContent += characters[secondRandom]
+            firstPassword.textContent += allCharacters[firstRandom]
+            secondPassword.textContent += allCharacters[secondRandom]
         }
         isActive = true
 
     
 }
+
+
+
 
 function copyPassword(elementId) 
 {
